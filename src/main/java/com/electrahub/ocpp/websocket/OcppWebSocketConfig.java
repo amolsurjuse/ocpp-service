7f0556c5eecbc3150325641227c8e66aca613cfa
amolsurjuse <amol@electrahub.com>
2026-03-20 15:36:54 -0400

    fix: restore Bean import for websocket container config

--- src/main/java/com/electrahub/ocpp/websocket/OcppWebSocketConfig.java
+++ src/main/java/com/electrahub/ocpp/websocket/OcppWebSocketConfig.java
@@ -1,6 +1,7 @@
 package com.electrahub.ocpp.websocket;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
