b75a344e406752857b83b39e78c97e622f035b31
amolsurjuse <amol@electrahub.com>
2026-03-20 15:34:58 -0400

    fix: import OcppMessageRouter in websocket config

--- src/main/java/com/electrahub/ocpp/websocket/OcppWebSocketConfig.java
+++ src/main/java/com/electrahub/ocpp/websocket/OcppWebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.electrahub.ocpp.websocket;
 
+import com.electrahub.ocpp.service.OcppMessageRouter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
